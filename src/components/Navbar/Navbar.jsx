dc3b4399d3636f510f6bfc505da3e757771374c2
Md. Alamgir <agir97@gmail.com>
2023-09-25 11:05:57 +0600

    fourth commit: Updated Navbar buttons with dynamic NavLinks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
-
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="max-w-screen-lg mx-auto flex justify-between mt-12 bg-pink-50 p-2">             
              <img className="h-12" src="src\assets\Logo.png" alt="" />
         <div className=" flex gap-4">
-          <button to="/">Home</button>
-          <button to="/home">Donation</button>
-          <button to="/statistics">Statistics</button>
+
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/donation">Donation</NavLink>
+          <NavLink to="/statistics">Statistics</NavLink>
+          
         </div>
         </div>
     );
